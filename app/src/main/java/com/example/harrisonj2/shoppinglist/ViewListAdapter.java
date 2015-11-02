29ff66b9cea7fe408e2718cd1056f4ac6dd97089
AlexanderB92 <a.m.brandstrup@gmail.com>
2015-11-02 10:03:37 -0500

    Custom row for viewing lists done

--- app/src/main/java/com/example/harrisonj2/shoppinglist/ViewListAdapter.java
+++ app/src/main/java/com/example/harrisonj2/shoppinglist/ViewListAdapter.java
@@ -1,7 +1,11 @@
 package com.example.harrisonj2.shoppinglist;
 
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 /**
  * Created by Alexander on 11/2/2015.
@@ -9,6 +13,11 @@ import android.widget.ArrayAdapter;
 public class ViewListAdapter extends ArrayAdapter<ShoppingList> {
 
     ShoppingList shoppingList;
+
+    TextView nameTextView;
+    TextView storeNameTextView;
+    TextView tripDateTextView;
+
 
 
     public ViewListAdapter(Context context, ShoppingList[] objects) {
@@ -21,8 +30,17 @@ public class ViewListAdapter extends ArrayAdapter<ShoppingList> {
 
         View shoppingListView = inflater.inflate(R.layout.custom_row, parent, false);
 
-        ShoppingList = getItem(position);
+        shoppingList = getItem(position);
 
+        nameTextView = (TextView) shoppingListView.findViewById(R.id.shoppingListNameText);
+        storeNameTextView = (TextView) shoppingListView.findViewById(R.id.storeNameText);
+        tripDateTextView = (TextView) shoppingListView.findViewById(R.id.tripDateText);
+
+        nameTextView.setText(shoppingList.getListName());
+        storeNameTextView.setText(shoppingList.getStoreName());
+        tripDateTextView.setText(shoppingList.getTripDate());
+
+        return shoppingListView;
 
     }
 
