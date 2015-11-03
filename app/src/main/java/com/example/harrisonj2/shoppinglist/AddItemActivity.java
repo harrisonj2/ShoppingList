96b03cd7d0ddfc551a00210fa30fd21e47c8025b
Julia <harrisonj2@chc.edu>
2015-11-03 14:26:06 -0500

    Code to use name sent to Add Item page to display list name

--- app/src/main/java/com/example/harrisonj2/shoppinglist/AddItemActivity.java
+++ app/src/main/java/com/example/harrisonj2/shoppinglist/AddItemActivity.java
@@ -1,18 +1,34 @@
 package com.example.harrisonj2.shoppinglist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    TextView listNameTextView;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        String message;
+        listNameTextView = (TextView) findViewById(R.id.listNameTextView);
+        Bundle name = getIntent().getExtras();
+        if(name==null)
+            message = "";
+        else
+            message = name.getString("name");
+
+        listNameTextView.setText(message);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
