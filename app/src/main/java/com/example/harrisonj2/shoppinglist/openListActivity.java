065e2909defd056686779656e675c476a11b1f8e
Julia <harrisonj2@chc.edu>
2015-11-18 13:53:49 -0500

    Notification Feature. Not completely working

--- app/src/main/java/com/example/harrisonj2/shoppinglist/openListActivity.java
+++ app/src/main/java/com/example/harrisonj2/shoppinglist/openListActivity.java
@@ -1,9 +1,12 @@
 package com.example.harrisonj2.shoppinglist;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -26,10 +29,20 @@ public class openListActivity extends AppCompatActivity {
     DBHandler dbHandler;
     ListAdapter adapter;
 
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 456852;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_list);
+
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+
+        if(allGotten(this)){
+            notification(this);
+        }
 
         Bundle extras = getIntent().getExtras();
         if(extras != null) {
@@ -120,4 +133,33 @@ public class openListActivity extends AppCompatActivity {
         costTextView.setText(message);
     }
 
+    public void notification(openListActivity view){
+        notification.setSmallIcon(R.mipmap.ic_launcher);
+        notification.setTicker("List Complete");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("List Complete");
+        notification.setContentText("All items in " + shoppingListName + " have been purchased.");
+
+        intent = new Intent(this, ViewListActivity.class);
+        PendingIntent pend = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pend);
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID, notification.build());
+    }
+
+    public boolean allGotten(openListActivity view){
+        boolean gotten = true;
+
+        if(itemData != null){
+            for (int i = 0; i < itemData.length; i++){
+                if(itemData[i].getPurchased().equals("false"))
+                    gotten = false;
+            }
+        }else
+            gotten = false;
+
+
+        return gotten;
+    }
 }
